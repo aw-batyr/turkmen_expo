346e3d12d1bcf019c9b73ea630cac974378b9480
Kakabay <2kakabayashyrberdyew@gmail.com>
2024-04-15 18:34:40 +0500

    calendar dcard changed

--- components/cards/EventCard.tsx
+++ components/cards/EventCard.tsx
@@ -1,11 +1,11 @@
-import React from "react";
-import Link from "next/link";
-import Image from "next/image";
-import clsx from "clsx";
-import "@/app/styles/hover.css";
+import React from 'react';
+import Link from 'next/link';
+import Image from 'next/image';
+import clsx from 'clsx';
+import '@/app/styles/hover.css';
 
-import calendarImg from "@/public/assets/icons/calendar.svg";
-import { Timing } from "@/lib/types/Calendar.type";
+import calendarImg from '@/public/assets/icons/calendar.svg';
+import { Timing } from '@/lib/types/Calendar.type';
 
 interface Props {
   id: number;
@@ -37,9 +37,9 @@ export const EventCard = ({
   dark = false,
 }: Props) => {
   const formatDate = (dateString: string) => {
-    const dateParts = dateString.split(" ");
+    const dateParts = dateString.split(' ');
     const date = dateParts[0];
-    const parts = date.split("-");
+    const parts = date.split('-');
     const formattedDate = `${parts[2]}.${parts[1]}.${parts[0]}`;
 
     return formattedDate;
@@ -47,31 +47,28 @@ export const EventCard = ({
 
   return (
     <>
-      <Link className="w-full" href={`/calendar/${id}`} passHref>
+      <div
+        className="w-full cursor-default"
+        // href={`/calendar/${id}`}
+        // passHref
+      >
         <div
           className={clsx(
-            "bg-bgWhite p-6 min-[850px]:p-[40px] lg:p-[60px] rounded-[2px] cursor-pointer transition-all w-full greenBtnShadow hover:hover-shadow",
+            'bg-bgWhite p-6 min-[850px]:p-[40px] lg:p-[60px] rounded-[2px] transition-all w-full greenBtnShadow hover:hover-shadow',
             {
-              "bg-navyBlue text-gray": dark,
-            }
-          )}
-        >
+              'bg-navyBlue text-gray': dark,
+            },
+          )}>
           <div
             className={`md:hidden flex flex-col gap-[10px] border-b-[1px] mb-5 ${
-              dark ? "border-gray3" : "border-lightCyan"
-            } pb-5 md:gap-y-[20px] min-w-[200px]`}
-          >
+              dark ? 'border-gray3' : 'border-lightCyan'
+            } pb-5 md:gap-y-[20px] min-w-[200px]`}>
             <div
-              className={clsx(
-                "text-[16px] flex leading-[150%] md:text-[18px] font-semibold",
-                {
-                  "text-red": !dark,
-                  "text-lightYellow": dark,
-                }
-              )}
-            >
-              <p className="mr-5">{formatDate(starts)}</p>{" "}
-              <p>{formatDate(ends)}</p>
+              className={clsx('text-[16px] flex leading-[150%] md:text-[18px] font-semibold', {
+                'text-red': !dark,
+                'text-lightYellow': dark,
+              })}>
+              <p className="mr-5">{formatDate(starts)}</p> <p>{formatDate(ends)}</p>
             </div>
           </div>
 
@@ -82,25 +79,20 @@ export const EventCard = ({
                 <p className="text-[12px] text-gray">{category}</p>
                 <div className="flex flex-col gap-[10px] md:gap-[15px]">
                   <h3
-                    className={clsx(
-                      "text-[21px] font-bold leading-[115%] md:leading-[100%]",
-                      {
-                        "text-bgWhite": dark,
-                        "text-green": !dark,
-                      }
-                    )}
-                  >
+                    className={clsx('text-[21px] font-bold leading-[115%] md:leading-[100%]', {
+                      'text-bgWhite': dark,
+                      'text-green': !dark,
+                    })}>
                     {title}
                   </h3>
                   <p
                     className={clsx(
-                      "text-[16px] event-text text-black leading-[130%] md:leading-[150%]",
+                      'text-[16px] event-text text-black leading-[130%] md:leading-[150%]',
                       {
-                        "text-gray2 md:text-black": !dark,
-                        "text-gray4": dark,
-                      }
-                    )}
-                  >
+                        'text-gray2 md:text-black': !dark,
+                        'text-gray4': dark,
+                      },
+                    )}>
                     {description}
                   </p>
                 </div>
@@ -115,20 +107,16 @@ export const EventCard = ({
 
             <div className="hidden md:flex flex-col gap-[10px] md:gap-y-[20px] ">
               <div
-                className={clsx(
-                  "text-[16px] leading-[150%] md:text-[18px] font-semibold",
-                  {
-                    "text-red": !dark,
-                    "text-lightYellow": dark,
-                  }
-                )}
-              >
+                className={clsx('text-[16px] leading-[150%] md:text-[18px] font-semibold', {
+                  'text-red': !dark,
+                  'text-lightYellow': dark,
+                })}>
                 {formatDate(starts)} <br /> {formatDate(ends)}
               </div>
             </div>
           </div>
         </div>
-      </Link>
+      </div>
     </>
   );
 };
