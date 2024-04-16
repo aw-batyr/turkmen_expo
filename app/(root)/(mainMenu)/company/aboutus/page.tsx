c83579798c6e6ff798caeb75f5ddb9afadbba76b
batyr-Ashyrberdiyev <awbatyr@gmail.com>
2024-04-16 17:58:40 +0500

    add fetch about page

--- app/(root)/(mainMenu)/company/aboutus/page.tsx
+++ app/(root)/(mainMenu)/company/aboutus/page.tsx
@@ -1,14 +1,42 @@
 "use client";
 
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Image from "next/image";
 
 import aboutImg from "@/public/assets/images/about.png";
 import { aboutData } from "@/lib/database/about";
 import { v4 } from "uuid";
 import { LayoutWithSidebar } from "@/components/page/LayoutWithSidebar";
+import { baseAPI } from "@/lib/API";
+import { useAppSelector } from "@/redux/hooks";
+import { selectHeader } from "@/redux/slices/headerSlice";
 
 const About = () => {
+  // const [aboutData, setAboutData] = useState<any>();
+  // const { activeLang } = useAppSelector(selectHeader);
+
+  // const fecthAboutData = async () => {
+  //   try {
+  //     const res = await fetch(
+  //       `${baseAPI}settings/about_us?X-Localization=${activeLang.localization}`
+  //     );
+
+  //     if (!res.ok) {
+  //       throw new Error("Error");
+  //     }
+
+  //     const data = await res.json();
+
+  //     setAboutData(data);
+  //   } catch (error) {
+  //     console.error(error);
+  //   }
+  // };
+
+  // useEffect(() => {
+  //   fecthAboutData();
+  // }, []);
+
   return (
     <LayoutWithSidebar second="Коротко о нас" title="Коротко о нас">
       <Image src={aboutImg} width={1000} height={500} alt="about image" />
