ccd6227fe5cd34f460d63f41489cf594b44c6999
batyr-Ashyrberdiyev <awbatyr@gmail.com>
2024-04-16 18:01:35 +0500

    change path

--- components/home/Video.tsx
+++ components/home/Video.tsx
@@ -1,31 +1,10 @@
-'use client';
-
-import React, { useEffect, useState } from 'react';
-
 export const Video = () => {
-  const [video, setVideo] = useState();
-
-  const fetchVideo = async () => {
-    try {
-      const res = await fetch(`https://turkmenexpo.com/app/storage/app/media/video/video.mp4`);
-
-      const data = await res.json();
-      console.log(data);
-      setVideo(data);
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
-  //   useEffect(() => {
-  //     fetchVideo();
-  //   }, []);
-
   return (
     <video
       className="w-full mx-auto "
       autoPlay
-      src="https://turkmenexpo.com/app/storage/app/media/video/video.mp4"
-      typeof="video/mp4"></video>
+      src="/assets/video.mp4"
+      typeof="video/mp4"
+    ></video>
   );
 };
