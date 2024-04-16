faa04cb1edeb00dd802c88dbe8d83be208237931
batyr-Ashyrberdiyev <awbatyr@gmail.com>
2024-04-16 18:01:46 +0500

    add animate

--- components/ui/BurgerMenu.tsx
+++ components/ui/BurgerMenu.tsx
@@ -60,12 +60,12 @@ export const BurgerMenu = () => {
 
   return (
     <motion.div
-      // initial={{ x: '100%' }}
-      // animate={{ x: 0 }}
-      // transition={{
-      //   duration: 0.4,
-      //   ease: 'circOut',
-      // }}
+      initial={{ x: "100%" }}
+      animate={{ x: 0 }}
+      transition={{
+        duration: 0.4,
+        ease: "circOut",
+      }}
       className="bg-green overflow-auto fixed w-full z-[900] top-[74px] bottom-0 left-0 min-h-[100vh] h-full px-4 py-10 flex flex-col gap-10 overflow-y-auto"
     >
       <div className="flex flex-col gap-5">
