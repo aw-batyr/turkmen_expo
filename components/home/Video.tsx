b83a65b595741dd85710050076c797fc3fcb0f83
Kakabay <2kakabayashyrberdyew@gmail.com>
2024-04-18 15:49:54 +0500

    adding loading status

--- components/home/Video.tsx
+++ components/home/Video.tsx
@@ -1,5 +1,32 @@
+'use client';
+
+import Image from 'next/image';
+import { useState } from 'react';
+
+import vid from '@/public/assets/images/video.png';
+import clsx from 'clsx';
+
 export const Video = () => {
+  const [loading, setLoading] = useState(true);
+
+  console.log(loading);
+
   return (
-    <video className="w-full mx-auto" autoPlay src="/assets/video.mp4"></video>
+    <div className="relative">
+      {loading && (
+        <Image
+          src={vid}
+          alt="video"
+          className="absolute top-0 bottom-0 w-full h-[565px] object-cover mx-auto"
+          onLoad={() => setLoading(false)}
+        />
+      )}
+      <video
+        className={clsx('w-full h-[565px] object-cover mx-auto', {})}
+        autoPlay
+        src="/assets/video.mp4"
+        onLoad={() => setLoading(false)}
+        onLoadedData={() => setLoading(false)}></video>
+    </div>
   );
 };
