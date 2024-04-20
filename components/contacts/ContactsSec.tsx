a119e3dce088c2a3c9b56109a362e8f0f338f038
Kakabay <2kakabayashyrberdyew@gmail.com>
2024-04-20 17:36:42 +0500

    add fetch contacts

--- components/contacts/ContactsSec.tsx
+++ components/contacts/ContactsSec.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { v4 } from 'uuid';
 
 import { contacBottomData, innerContactData, menuData } from '@/lib/database/contactsData';
@@ -8,32 +8,64 @@ import { ContactsDropDown } from './ContactsDropDown';
 
 import { useAppDispatch } from '@/redux/hooks';
 import { setActiveMenu, setContactTitle } from '@/redux/slices/contactsSlice';
+import { baseAPI } from '@/lib/API';
+import { ContactsDataType } from '@/lib/types/Contacts.type';
 
 export const ContactsSec = () => {
+  const [contactsData, setContactsData] = useState<ContactsDataType>();
   const dispatch = useAppDispatch();
 
   const openContacts = (name: string) => {
     dispatch(setContactTitle(name));
-    dispatch(setActiveMenu(name)); // Set the active menu to the clicked menu's name
+    dispatch(setActiveMenu(name));
   };
+
+  const fetchContactsData = async () => {
+    try {
+      const res = await fetch(`${baseAPI}contacts`);
+
+      if (!res.ok) {
+        throw new Error('Error');
+      }
+
+      const data = await res.json();
+
+      setContactsData(data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchContactsData();
+  }, []);
+
+  console.log(contactsData);
 
   return (
     <div className="flex flex-col w-full">
-      {menuData.map((obj) => (
+      {/* {menuData.map((obj) => (
         <ContactsDropDown key={v4()} openContacts={openContacts} {...obj} />
-      ))}
+      ))} */}
       <div className="flex flex-col items-start mt-[20px] w-full">
-        {contacBottomData.map((item) => (
-          <div className="py-10 sm:py-[30px] border-b-[1px] border-navyBlue5 w-full" key={v4()}>
-            <h4 className="leading-[120%] sm:leading-[100%] text-[16px] sm:text-[21px] mb-6">
-              {item.title}
-            </h4>
-            <div className="text-gray4 sm:text-bgWhite flex flex-col items-start leading-[150%] text-[14px] sm:text-[16px]">
-              <p>{item.phone}</p>
-              <p>{item.email}</p>
-            </div>
-          </div>
-        ))}
+        {contactsData
+          ? contactsData.data.map((item) => (
+              <div className="py-10 sm:py-[30px] border-b-[1px] border-navyBlue5 w-full" key={v4()}>
+                <h4 className="leading-[120%] sm:leading-[100%] text-[16px] sm:text-[21px] mb-6">
+                  {item.header}
+                </h4>
+                <div className="text-gray4 sm:text-bgWhite flex flex-col items-start leading-[150%] text-[14px] sm:text-[16px]">
+                  {item.services.map((service) => (
+                    <>
+                      <p>{service.phone}</p>
+                      <p>{service.email}</p>
+                      <p>{service.web_site}</p>
+                    </>
+                  ))}
+                </div>
+              </div>
+            ))
+          : null}
       </div>
     </div>
   );
