e71b2506b148df26a628dde16633e4925d4b9453
batyr-Ashyrberdiyev <awbatyr@gmail.com>
2024-04-24 18:21:42 +0500

    commit

--- components/home/Video.tsx
+++ components/home/Video.tsx
@@ -1,25 +1,18 @@
-'use client';
-
-import Image from 'next/image';
-import { useState } from 'react';
-
-import clsx from 'clsx';
-import vid from '@/public/assets/images/video.png';
+"use client";
 
 export const Video = () => {
-  const [loading, setLoading] = useState(true);
-
-  console.log(loading);
-
   return (
     <div className="relative">
       <video
-        className={clsx('w-full lg:h-[565px] sm:h-[375px] h-[250px] object-cover mx-auto', {})}
+        className={
+          "w-full lg:h-[565px] sm:h-[375px] h-[250px] object-cover mx-auto"
+        }
         autoPlay
         loop
         muted
-        poster={'/assets/images/video.png'}
-        src="/assets/video.mp4"></video>
+        poster={"/assets/images/video.png"}
+        src="/assets/video.mp4"
+      ></video>
     </div>
   );
 };
