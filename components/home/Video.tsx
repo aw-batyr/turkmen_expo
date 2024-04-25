f396b12e8a43ee8df8da2b7e6803c1f98c6b9bc6
batyr-Ashyrberdiyev <awbatyr@gmail.com>
2024-04-25 18:10:09 +0500

    none

--- components/home/Video.tsx
+++ components/home/Video.tsx
@@ -1,18 +1,36 @@
 "use client";
 
+import clsx from "clsx";
+import { useState, useRef, useEffect } from "react";
+
 export const Video = () => {
+  const [loading, setLoading] = useState(false);
+  const vidRef = useRef<HTMLVideoElement>(null);
+
+  console.log(vidRef);
+
   return (
     <div className="relative">
       <video
-        className={
-          "w-full lg:h-[565px] sm:h-[375px] h-[250px] object-cover mx-auto"
-        }
+        className={clsx(
+          "w-full opacity-100 lg:h-[565px] sm:h-[375px] h-[250px] object-cover mx-auto",
+          {}
+        )}
+        id="vid"
+        ref={vidRef}
+        onWaiting={() => console.log("loading")}
         autoPlay
+        typeof="video/mp4"
         loop
         muted
-        poster={"/assets/images/video.png"}
+        // disablepictureinpicture
+        // disablePictureInPicture
         src="/assets/video.mp4"
-      ></video>
+      />
+      <div
+        id="spinner"
+        className="hidden border-[8px] border-solid border-l-red h-[60px] w-[60px] rounded-full z-[100] absolute top-[100px] left-[100px] "
+      ></div>
     </div>
   );
 };
