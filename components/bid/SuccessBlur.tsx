5736554fbece6cb11186176ffc69d22af9805de0
batyr-Ashyrberdiyev <awbatyr@gmail.com>
2024-05-22 16:14:15 +0500

    media added

--- components/bid/SuccessBlur.tsx
+++ components/bid/SuccessBlur.tsx
@@ -9,11 +9,11 @@ export const SuccessBlur = ({
   setModal: (str: string) => void;
 }) => {
   return (
-    <div className="fixed top-0 right-0 bottom-0 left-0 z-[3000] backdrop-blur-sm w-full h-screen bg-darkBlue/50">
+    <div className="fixed top-0 right-0 bottom-0 left-0 z-[3000] px-5 backdrop-blur-sm w-full h-screen bg-darkBlue/50">
       <div className="w-full max-w-[630px] top-[50%] relative translate-y-[-50%] mx-auto my-auto flex flex-col gap-6 text-center">
         <Title text="Ваша заявка принята" />
 
-        <p className="leading-[150%]">
+        <p className="leading-[150%] text-[13px] sm:text-[16px]">
           После отправки правильно заполненной формы с Вами свяжется менеджер,
           который будет курировать все вопросы по участию Вашей компании в
           выставке.
