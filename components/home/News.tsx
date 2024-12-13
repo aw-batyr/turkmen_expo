988c4319e6e1d02ce26a2fbcee304269c74bc2c7
batyr-Ashyrberdiyev <awbatyr@gmail.com>
2024-12-13 15:52:42 +0500

    loading error completed

--- components/home/News.tsx
+++ components/home/News.tsx
@@ -1,37 +1,40 @@
-'use client';
+"use client";
 
-import React from 'react';
-import { Swiper, SwiperSlide } from 'swiper/react';
-import { Navigation } from 'swiper/modules';
+import React, { useState } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation } from "swiper/modules";
 
-import { NewsCard } from '../cards/NewsCard';
-import { NavBtn } from './ui/NavBtn';
-import { GreenBtn } from '../ui/Buttons';
-import { Title } from './Title';
+import { NewsCard } from "../cards/NewsCard";
+import { NavBtn } from "./ui/NavBtn";
+import { GreenBtn } from "../ui/Buttons";
+import { Title } from "./Title";
 
-import { useAppSelector } from '@/redux/hooks';
-import { selectHeader } from '@/redux/slices/headerSlice';
-import { NewsData } from '@/lib/types/NewsData.type';
+import { useAppSelector } from "@/redux/hooks";
+import { selectHeader } from "@/redux/slices/headerSlice";
+import { NewsData } from "@/lib/types/NewsData.type";
 
-import 'swiper/css/bundle';
+import "swiper/css/bundle";
 
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
-import 'swiper/css/scrollbar';
-import Link from 'next/link';
-import { useLang } from '@/utils/useLang';
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
+import Link from "next/link";
+import { useLang } from "@/utils/useLang";
+import Loader from "../ui/Loader";
 
 export const News = () => {
   const [newsData, setNewsData] = React.useState<NewsData>();
+  const [loading, setLoading] = useState(true);
 
   const { activeLang } = useAppSelector(selectHeader);
 
   const fetchNews = async () => {
     try {
+      setLoading(true);
       const response = await fetch(`https://turkmenexpo.com/app/api/v1/news`, {
         headers: {
-          'Accept-Language': activeLang.localization,
+          "Accept-Language": activeLang.localization,
         },
       });
 
@@ -42,7 +45,9 @@ export const News = () => {
       const data = await response.json();
       setNewsData(data);
     } catch (error) {
-      console.error('Fetch error:', error);
+      console.error("Fetch error:", error);
+    } finally {
+      setLoading(false);
     }
   };
 
@@ -50,11 +55,15 @@ export const News = () => {
     fetchNews();
   }, [activeLang.localization]);
 
+  if (loading) {
+    <Loader className="h-[300px] w-full mx-auto" />;
+  }
+
   return (
     <>
       <div className="container w-full">
         <header className="flex items-center mb-5 sm:mb-[43px] justify-between">
-          <Title text={useLang('News', 'Новости', activeLang.localization)} />
+          <Title text={useLang("News", "Новости", activeLang.localization)} />
           <div className="hidden sm:flex items-center gap-5">
             <NavBtn left />
             <NavBtn />
@@ -74,25 +83,32 @@ export const News = () => {
               440: { slidesPerView: 1.5 },
             }}
             navigation={{
-              nextEl: '.next-btn',
-              prevEl: '.prev-btn',
-            }}>
-            {newsData?.data.map((item, i) => (
-              <SwiperSlide key={i}>
-                <NewsCard
-                  id={item.id}
-                  title={item.title}
-                  date={item.published_at}
-                  img={item.featured_images[0].path}
-                />
-              </SwiperSlide>
-            ))}
+              nextEl: ".next-btn",
+              prevEl: ".prev-btn",
+            }}
+          >
+            {loading ? (
+              <Loader className="h-[300px]" />
+            ) : (
+              newsData?.data.map((item, i) => (
+                <SwiperSlide key={i}>
+                  <NewsCard
+                    id={item.id}
+                    title={item.title}
+                    date={item.published_at}
+                    img={item.featured_images[0].path}
+                  />
+                </SwiperSlide>
+              ))
+            )}
           </Swiper>
         </div>
 
         <footer className="hidden sm:flex justify-center">
-          <Link href={'/news'}>
-            <GreenBtn text={useLang('All news', 'Все новости', activeLang.localization)} />
+          <Link href={"/news"}>
+            <GreenBtn
+              text={useLang("All news", "Все новости", activeLang.localization)}
+            />
           </Link>
         </footer>
       </div>
