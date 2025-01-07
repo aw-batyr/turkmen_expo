1512056d4ee5eb49b0cff09fca15cb65b5135b1c
Kakabay <2kakabayashyrberdyew@gmail.com>
2025-01-07 18:18:45 +0500

    added loading

--- components/calendar/CalendarSec.tsx
+++ components/calendar/CalendarSec.tsx
@@ -1,24 +1,25 @@
-'use client';
+"use client";
 
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 
-import { EventCard } from '../cards/EventCard';
+import { EventCard } from "../cards/EventCard";
 
-import { useAppDispatch, useAppSelector } from '@/redux/hooks';
-import { selectHeader } from '@/redux/slices/headerSlice';
-import { Title } from '../home/Title';
-import { setBurgerOpen } from '@/redux/slices/burgerSlice';
-import { baseAPI } from '@/lib/API';
-import { CalendarType } from '@/lib/types/Calendar.type';
-import { BreadCrumbs } from '../ui/BreadCrumbs';
-import { useLang } from '@/utils/useLang';
-import Loader from '../ui/Loader';
-import { Pagination } from '../ui/Pagination';
+import { useAppDispatch, useAppSelector } from "@/redux/hooks";
+import { selectHeader } from "@/redux/slices/headerSlice";
+import { Title } from "../home/Title";
+import { setBurgerOpen } from "@/redux/slices/burgerSlice";
+import { baseAPI } from "@/lib/API";
+import { CalendarType } from "@/lib/types/Calendar.type";
+import { BreadCrumbs } from "../ui/BreadCrumbs";
+import { useLang } from "@/utils/useLang";
+import Loader from "../ui/Loader";
+import { Pagination } from "../ui/Pagination";
 
 export const CalendarSec = ({}: {}) => {
   const { activeLang } = useAppSelector(selectHeader);
   const [eventsData, setEventsData] = useState<CalendarType>();
   const [current, setCurrent] = useState(1);
+  const [loading, setLoading] = useState(true);
   const dispatch = useAppDispatch();
 
   useEffect(() => {
@@ -27,19 +28,21 @@ export const CalendarSec = ({}: {}) => {
 
   const fetchEvents = async () => {
     try {
+      setLoading(true);
       const response = await fetch(`${baseAPI}expoevents?page=${current}`, {
         headers: {
-          'Accept-Language': activeLang.localization,
+          "Accept-Language": activeLang.localization,
         },
       });
 
       const data = await response.json();
 
       if (!response.ok) {
-        throw new Error('error');
+        throw new Error("error");
       }
 
       setEventsData(data);
+      setLoading(false);
     } catch (error) {
       console.error(error);
     }
@@ -49,6 +52,10 @@ export const CalendarSec = ({}: {}) => {
     fetchEvents();
   }, [activeLang.localization, current]);
 
+  if (loading) {
+    return <Loader className="h-[500px] mx-auto w-full " />;
+  }
+
   return (
     <div className="section-mb">
       <div className="container flex flex-col items-start pt-[20px] gap-10 md:gap-12">
@@ -56,19 +63,25 @@ export const CalendarSec = ({}: {}) => {
           <div className="mb-[24px]">
             <BreadCrumbs
               second={useLang(
-                'Calendar of events',
-                'Календарь мероприятий',
-                activeLang.localization,
+                "Calendar of events",
+                "Календарь мероприятий",
+                activeLang.localization
               )}
             />
           </div>
           <Title
-            text={useLang('Calendar of events', 'Календарь мероприятий', activeLang.localization)}
+            text={useLang(
+              "Calendar of events",
+              "Календарь мероприятий",
+              activeLang.localization
+            )}
           />
         </div>
         <div className="flex flex-col gap-6 w-full">
           {eventsData ? (
-            eventsData.data.map((item, i) => <EventCard dark={true} key={i} {...item} />)
+            eventsData.data.map((item, i) => (
+              <EventCard dark={true} key={i} {...item} />
+            ))
           ) : (
             <Loader className="h-[500px]" />
           )}
