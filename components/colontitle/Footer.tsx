0b0e7a6879293bb1737e2e477e54fd2a7f4bd688
Kakabay <2kakabayashyrberdyew@gmail.com>
2025-01-15 14:41:17 +0500

    socials added

--- components/colontitle/Footer.tsx
+++ components/colontitle/Footer.tsx
@@ -1,24 +1,31 @@
-'use client';
+"use client";
 
-import React from 'react';
-import Link from 'next/link';
+import React from "react";
+import Link from "next/link";
 
-import vk from '@/public/assets/icons/footer/vk.svg';
-import rss from '@/public/assets/icons/footer/rss.svg';
-import telegram from '@/public/assets/icons/footer/telegram.svg';
+import { footerInfo, headerMenu2 } from "@/lib/database/pathnames";
+import { useAppSelector } from "@/redux/hooks";
 
-import { footerInfo, headerMenu2 } from '@/lib/database/pathnames';
-import { useAppSelector } from '@/redux/hooks';
-import { useLang } from '@/utils/useLang';
-
-export const icons = [
-  { title: telegram, link: '' },
-  { title: vk, link: '' },
-  { title: rss, link: '' },
+export const socials = [
+  { href: "https://www.linkedin.com/company/turkmen-expo", icon: "linkedin" },
+  {
+    href: "https://www.facebook.com/profile.php?id=61567254728028",
+    icon: "facebook",
+  },
+  {
+    href: "https://www.instagram.com/turkmenexpo_tm?igsh=bnhkOWpmNWcwcHBq",
+    icon: "instagram",
+  },
+  {
+    href: "https://x.com/turkmenexpo?t=D-XSa8d0AC8GAv5peAzteA&s=09",
+    icon: "x",
+  },
 ];
 
 export const Footer = () => {
-  const localization = useAppSelector((state) => state.headerSlice.activeLang.localization);
+  const localization = useAppSelector(
+    (state) => state.headerSlice.activeLang.localization
+  );
 
   return (
     <footer className="bg-[#059784] text-white pt-6 pb-5 mob:py-[40px]">
@@ -29,7 +36,7 @@ export const Footer = () => {
 
             <div className="flex flex-col mt-8 lg:flex-row items-center font-medium gap-12">
               {headerMenu2
-                .filter((item) => (localization === 'en' ? item.en : !item.en))
+                .filter((item) => (localization === "en" ? item.en : !item.en))
                 .map((item, i) => (
                   <Link key={i} href={item.link} className="cursor-pointer">
                     {item.title}
@@ -40,7 +47,7 @@ export const Footer = () => {
 
           <div className="flex flex-col lg:flex-row lg:justify-between justify-center text-center lg:text-left lg:items-end items-center gap-6">
             <div className="flex flex-col justify-end w-full">
-              {localization === 'ru' ? (
+              {localization === "ru" ? (
                 <div className="flex flex-col md:gap-y-[10px] gap-0">
                   {footerInfo.slice(0, 3).map((item, i) => (
                     <p className="text-[12px] leading-[130%]" key={i}>
@@ -48,7 +55,7 @@ export const Footer = () => {
                     </p>
                   ))}
                 </div>
-              ) : localization === 'en' ? (
+              ) : localization === "en" ? (
                 <div className="flex flex-col md:gap-y-[10px] gap-0">
                   {footerInfo.slice(3, 7).map((item, i) => (
                     <p className="text-[12px] leading-[130%]" key={i}>
@@ -59,22 +66,24 @@ export const Footer = () => {
               ) : null}
             </div>
 
-            <div className="flex items-center lg:justify-end justify-center gap-2.5 w-full">
-              <h5>{useLang('Social Media:', 'Социальные сети:', localization)}</h5>
-              <div className="flex items-center gap-4">
-                <a href="">
-                  <img src="/assets/icons/instagram.svg" alt="social" />
+            <div className="flex items-center gap-6">
+              {socials.map((item, i) => (
+                <a
+                  key={i}
+                  href={item.href}
+                  target="_blank"
+                  className="size-8 p-1"
+                >
+                  <img
+                    src={`/assets/icons/${item.icon}.svg`}
+                    alt="social"
+                    className="size-full object-contain"
+                  />
                 </a>
-              </div>
+              ))}
             </div>
           </div>
         </div>
-
-        {/* <div className="text-[#435654] flex mob:flex-row flex-col items-center justify-between pt-[17px] border-t-[1px] border-gray3">
-          <p className="mob:text-[12px] mx-auto text-[12px] leading-[130%] md:leading-[150%] md:mb-0 mb-5">
-            {useLang('@2024 IE TurkmenExpo', '@2024 ИП ТуркменЭкспо', localization)}
-          </p>
-        </div> */}
       </div>
     </footer>
   );
