3814ed20b2a28bde214ab40a97e92193cab27e41
Kakabay <2kakabayashyrberdyew@gmail.com>
2025-01-15 09:17:28 +0500

    suspense added

--- components/colontitle/Header.tsx
+++ components/colontitle/Header.tsx
@@ -1,23 +1,23 @@
-'use client';
+"use client";
 
-import React, { useEffect } from 'react';
-import clsx from 'clsx';
-import Link from 'next/link';
-import Image from 'next/image';
-import { AnimatePresence } from 'framer-motion';
+import React, { Suspense, useEffect } from "react";
+import clsx from "clsx";
+import Link from "next/link";
+import Image from "next/image";
+import { AnimatePresence } from "framer-motion";
 
-import logo from '@/public/assets/icons/logo.svg';
-import search from '@/public/assets/icons/header/search.svg';
-import searchMob from '@/public/assets/icons/header/mob-search.svg';
+import logo from "@/public/assets/icons/logo.svg";
+import search from "@/public/assets/icons/header/search.svg";
+import searchMob from "@/public/assets/icons/header/mob-search.svg";
 
-import { LangMenu } from '../ui/LangMenu';
-import { SearchInput } from '../home/search-input';
-import { headerMenu2 } from '@/lib/database/pathnames';
-import { BurgerMenu } from '../ui/BurgerMenu';
-import { useAppDispatch, useAppSelector } from '@/redux/hooks';
-import { selectHeader, setShowInput } from '@/redux/slices/headerSlice';
-import { selectBurger, setBurgerOpen } from '@/redux/slices/burgerSlice';
-import { useStorage } from '@/hooks/useStorage';
+import { LangMenu } from "../ui/LangMenu";
+import { SearchInput } from "../home/search-input";
+import { headerMenu2 } from "@/lib/database/pathnames";
+import { BurgerMenu } from "../ui/BurgerMenu";
+import { useAppDispatch, useAppSelector } from "@/redux/hooks";
+import { selectHeader, setShowInput } from "@/redux/slices/headerSlice";
+import { selectBurger, setBurgerOpen } from "@/redux/slices/burgerSlice";
+import { useStorage } from "@/hooks/useStorage";
 
 export const Header = () => {
   const dispatch = useAppDispatch();
@@ -35,7 +35,7 @@ export const Header = () => {
     dispatch(setBurgerOpen(false));
   };
 
-  const { setItem } = useStorage('language');
+  const { setItem } = useStorage("language");
 
   useEffect(() => {
     setItem(activeLang);
@@ -55,11 +55,12 @@ export const Header = () => {
 
       <header
         className={clsx(
-          'bg-bgWhite border-b border-[#DFE2E1] tab:hidden relative z-[50] flex items-center justify-between px-4 py-6',
+          "bg-bgWhite border-b border-[#DFE2E1] tab:hidden relative z-[50] flex items-center justify-between px-4 py-6",
           {
             // 'fixed w-full top-0': burgerOpen,
-          },
-        )}>
+          }
+        )}
+      >
         <Image
           src={searchMob}
           height={32}
@@ -74,29 +75,43 @@ export const Header = () => {
             dispatch(setBurgerOpen(false));
             dispatch(setShowInput(false));
           }}
-          href={'/'}>
-          <Image src={logo} height={24} width={160} alt="лого" className="cursor-pointer" />
+          href={"/"}
+        >
+          <Image
+            src={logo}
+            height={24}
+            width={160}
+            alt="лого"
+            className="cursor-pointer"
+          />
         </Link>
 
         <div
           onClick={toggleMenu}
-          className="cursor-pointer h-8 w-8 flex flex-col p-1 justify-between items-center">
+          className="cursor-pointer h-8 w-8 flex flex-col p-1 justify-between items-center"
+        >
           <span
-            className={clsx('block transition-all rounded-full bg-[#059784] w-6 h-[2px]', {
-              'rotate-[45deg] translate-y-[9px]': burgerOpen,
-            })}
-          />
-          <span
-            className={clsx('block transition-all rounded-full bg-[#059784] w-6 h-[2px]', {
-              'opacity-0 hidden': burgerOpen,
-            })}
+            className={clsx(
+              "block transition-all rounded-full bg-[#059784] w-6 h-[2px]",
+              {
+                "rotate-[45deg] translate-y-[9px]": burgerOpen,
+              }
+            )}
           />
           <span
             className={clsx(
-              'block transition-all duration-300 rounded-full bg-[#059784] w-6 h-[2px]',
+              "block transition-all rounded-full bg-[#059784] w-6 h-[2px]",
               {
-                'rotate-[-45deg] translate-y-[-10px]': burgerOpen,
-              },
+                "opacity-0 hidden": burgerOpen,
+              }
+            )}
+          />
+          <span
+            className={clsx(
+              "block transition-all duration-300 rounded-full bg-[#059784] w-6 h-[2px]",
+              {
+                "rotate-[-45deg] translate-y-[-10px]": burgerOpen,
+              }
             )}
           />
         </div>
@@ -144,7 +159,9 @@ export const Header = () => {
                 <Image src={logo} alt="logo" height={38} width={235} />
               </Link>
               <div className="flex gap-[10px]">
-                <LangMenu />
+                <Suspense>
+                  <LangMenu />
+                </Suspense>
                 <Image
                   src={search}
                   alt="поиск"
@@ -156,7 +173,9 @@ export const Header = () => {
 
             <div className="flex items-center gap-x-5 font-medium">
               {headerMenu2
-                .filter((item) => (activeLang.localization === 'en' ? item.en : !item.en))
+                .filter((item) =>
+                  activeLang.localization === "en" ? item.en : !item.en
+                )
                 .map((item, i) => (
                   <Link key={i} href={item.link} className="cursor-pointer">
                     {item.title}
