ab525e0d43ef56f874d53775817c090b71175244
Kakabay <2kakabayashyrberdyew@gmail.com>
2025-01-16 20:59:07 +0500

    renamed

--- components/home/Title.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export const Title = ({ text }: { text: string | boolean }) => {
-  return (
-    <h2 className={`text-[26px] sm:text-[34px] leading-[115%] font-normal`}>
-      {text}
-    </h2>
-  );
-};
