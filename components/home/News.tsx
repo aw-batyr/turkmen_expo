d4f86d99d59b1fde2549307dc1419f6639ac8b94
Kakabay <2kakabayashyrberdyew@gmail.com>
2025-01-20 17:48:08 +0500

    renamed

--- components/home/News.tsx
+++ components/home/News.tsx
@@ -19,9 +19,9 @@ import "swiper/css/pagination";
 import "swiper/css/scrollbar";
 import Link from "next/link";
 import { useLang } from "@/utils/useLang";
-import Loader from "../ui/loader";
 import { GreenBtn } from "../ui/Buttons";
 import { Title } from "../ui/title";
+import Loader from "../ui/loader";
 
 export const News = () => {
   const [newsData, setNewsData] = useState<NewsData>();
