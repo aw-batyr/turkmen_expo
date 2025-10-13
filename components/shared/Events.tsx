30ad2533cb0a404684ebf16604af5abf13fa38dd
Batyr <awbatyr@gmail.com>
2025-10-13 15:50:04 +0500

    change

--- components/shared/Events.tsx
+++ components/shared/Events.tsx
@@ -1,9 +1,23 @@
-import { EventCard } from "./event-card";
-
 import { getEvents } from "@/services/home";
 import { EventsMobile } from "./events-mobile";
-import { LinkButton } from "../ui/link-button";
-import { Title } from "../ui/title";
+
+const events = [
+  {
+    video:
+      "https://editor.turkmenexpo.com/storage/app/media/video/KidsExpo%202024_%20Turkmenistan.mp4",
+    logo: "https://kids.turkmenexpo.com/logo.svg",
+  },
+  {
+    video:
+      "https://turkmentextile.turkmenexpo.com/app/storage/app/media/video/Textile2025.mp4",
+    logo: "/assets/icons/turkmen-textile.png",
+  },
+  {
+    video:
+      "https://itse.turkmenexpo.com/app/storage/app/media/video/itse2025.mp4",
+    logo: "https://itse.turkmenexpo.com/logo.svg",
+  },
+];
 
 export const Events = async ({ lang }: { lang: string }) => {
   const data = await getEvents(lang);
@@ -11,11 +25,19 @@ export const Events = async ({ lang }: { lang: string }) => {
   const btnText =
     lang === "en" ? "Show more" : lang === "ru" ? "Показать еще" : "Show more";
 
+  const title = lang === "en" ? "Exhibitions" : "Выставки";
+
+  const linkText = lang === "en" ? "Go to website" : "Перейти на сайт";
+
   return (
     <section>
-      <div className="container hidden md:block">
+      <div className="container md:block">
         <div className="sm:mb-10">
-          <Title
+          <h2 className="text-center font-semibold text-3xl mb-8">
+            {title} <span className="text-PRIMARY">Turkmen</span>
+            <span className="text-red">Expo</span>
+          </h2>
+          {/* <Title
             text={
               lang === "en"
                 ? "Upcoming exhibitions and events"
@@ -23,10 +45,11 @@ export const Events = async ({ lang }: { lang: string }) => {
                 ? "Ближайшие выставки и мероприятия"
                 : "Upcoming exhibitions and events"
             }
-          />
+          /> */}
         </div>
-        <div className="w-full flex flex-col items-center gap-8">
-          {data.data.slice(0, 2).map((item, i) => (
+        {/* <div className="w-full flex flex-col items-center gap-8"> */}
+        <div className="w-full flex flex-col items-center gap-8 lg:flex-row">
+          {/* {data.data.slice(0, 2).map((item, i) => (
             <EventCard
               coorganizers={item.coorganizers}
               dark
@@ -34,11 +57,43 @@ export const Events = async ({ lang }: { lang: string }) => {
               {...item}
             />
           ))}
-          <LinkButton href="/calendar">{btnText}</LinkButton>
+          <LinkButton href="/calendar">{btnText}</LinkButton> */}
+          {events.map((item, i) => (
+            <article
+              key={i}
+              className="w-full h-auto bg-white drop-shadow-sm rounded-sm"
+            >
+              <div className="w-full h-full">
+                <video
+                  src={item.video}
+                  className="size-full object-cover"
+                  autoPlay
+                  muted
+                  loop
+                />
+              </div>
+              <div className="flex items-center justify-between p-4">
+                <img
+                  height={80}
+                  width={100}
+                  src={item.logo}
+                  alt="logo"
+                  className="h-10 w-auto object-cover"
+                />
+                <a
+                  href={events[0].logo.slice(0, -8)}
+                  target="_blank"
+                  className="text-PRIMARY underline"
+                >
+                  {linkText}
+                </a>
+              </div>
+            </article>
+          ))}
         </div>
       </div>
 
-      <EventsMobile data={data.data.slice(0, 5)} />
+      {/* <EventsMobile data={data.data.slice(0, 5)} /> */}
     </section>
   );
 };
